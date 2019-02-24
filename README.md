3e7229aad478f4c9dea1e85ee992450d163bad60
musistudio <779956774@qq.com>
2019-02-24 17:46:45 +0800

    增加win10子系统的支持，并修复一些bug

--- README.md
+++ README.md
@@ -7,11 +7,19 @@
 sudo wget http://wukong-install.oss-cn-hangzhou.aliyuncs.com/install.sh && sudo chmod 777 install.sh &&./install.sh
 ```
 
-#### 说明 for Mac/Linux:
+#### 整体说明:
 1. 目前本项目只能用于MacOS/Linux/树莓派(Raspbian)系统的一键安装（仅在mac、Ubuntu16.04和树莓派(Raspbian)下测试通过
 2. 安装目录位于 ~/wukong-robot/
 3. 默认使用阿里云的源进行安装，如有问题请切换到github源进行安装
-4. 如遇到未知bug，请提Issues
+4. 运行后如果出现**No module named 'xxx’** 可能是有pip3的网络原因引起的，请切换到 ~/wukong-robot/ 目录后执行以下命令:
+```shell
+sudo pip3 install -r requirements.txt 
+```
+如果是win10子系统，请执行以下命令:
+```shell
+sudo pip3 install --upgrade --ignore-installed -r requirements.txt
+```
+5. 如遇到bug，请提Issues
 
 #### 说明 for raspberrypi:      
 1. 树莓派使用前请先换源，如果不知道怎么换源请执行以下命令:
@@ -21,5 +29,13 @@ sudo wget http://wukong-install.oss-cn-hangzhou.aliyuncs.com/changeSource.sh && 
 2. 增加选择录音和播放设备功能，如需使用请执行以下命令:
 ```shell
 sudo wget http://wukong-install.oss-cn-hangzhou.aliyuncs.com/choiceDevice.py && sudo python3 choiceDevice.py
+```
+
+### 说明 for win10's Ubuntu:
+1. win10子系统使用前需要在win10上开启pulseaudio-server      
+    下载解压 [pulseaudio.zip](http://wukong-install.oss-cn-hangzhou.aliyuncs.com/pulseaudio.zip) 并在CMD里运行bin目录下的 pulseaudio.exe
+2. 在子系统里(ubuntu)运行以下命令:
+```shell
+sudo wget http://wukong-install.oss-cn-hangzhou.aliyuncs.com/install.sh && sudo chmod 777 install.sh &&./install.sh
 ```
 
--- install.sh
+++ install.sh
@@ -33,7 +33,7 @@ install_wukong(){
         echo "正在为您安装wukong-robot,安装目录~/wukong-robot/"
         sudo apt-get update && sudo apt-get upgrade -y
         sudo git clone ${wukong} ~/wukong-robot
-        sudo apt-get install python-pyaudio python3-pyaudio sox pulseaudio libsox-fmt-all ffmpeg
+        sudo apt-get install python3 python3-pip python-pyaudio python3-pyaudio sox pulseaudio libsox-fmt-all ffmpeg
         sudo pip3 install --upgrade pip
         sudo pip3 install pyaudio distribute
         cd ~/wukong-robot
@@ -55,14 +55,13 @@ install_wukong(){
         sudo make
         sudo cp _snowboydetect.so ~/wukong-robot/snowboy/
         sudo rm -rf ~/install_temp/ ~/snowboy/
+        sudo chmod -R 777 ~/wukong-robot/
     elif [[ $os == "raspberrypi" ]]
     then
         echo "正在为您安装wukong-robot,安装目录~/wukong-robot/"
         sudo apt-get update && sudo apt-get  -y upgrade
-        sudo apt-get remove -y python3 python3-pip
-        sudo apt-get install -y python3 python3-pip
         sudo git clone ${wukong} ~/wukong-robot
-        sudo apt-get install -y python-pyaudio python3-pyaudio sox pulseaudio libsox-fmt-all ffmpeg
+        sudo apt-get install -y python3 python3-pip python-pyaudio python3-pyaudio sox pulseaudio libsox-fmt-all ffmpeg
         sudo pip3 install --upgrade pip
         cd ~/wukong-robot
         sudo pip3 install -r requirements.txt
@@ -81,6 +80,37 @@ install_wukong(){
         sudo make
         sudo cp _snowboydetect.so ~/wukong-robot/snowboy/
         sudo rm -rf ~/install_temp/ ~/snowboy/
+        sudo chmod -R 777 ~/wukong-robot/
+    elif [[ $os == "win10" ]]
+    then
+        echo "正在为您安装wukong-robot,安装目录~/wukong-robot/"
+        sudo add-apt-repository ppa:therealkenc/wsl-pulseaudio
+        sudo apt-get update && sudo apt-get upgrade -y
+        sudo git clone ${wukong} ~/wukong-robot
+        sudo apt-get install python3 python3-pip python-pyaudio python3-pyaudio sox pulseaudio libsox-fmt-all ffmpeg
+        sudo pip3 install --upgrade pip
+        sudo pip3 install pyaudio distribute
+        cd ~/wukong-robot
+        sudo pip3 install --upgrade --ignore-installed -r requirements.txt
+        sudo apt-get install python3-dev
+        sudo apt-get install python3-all-dev
+        sudo mkdir ~/install_temp && cd ~/install_temp
+        sudo wget http://hahack-1253537070.file.myqcloud.com/misc/swig-3.0.10.tar.gz
+        sudo tar xvf swig-3.0.10.tar.gz
+        cd swig-3.0.10
+        sudo apt-get install -y libpcre3 libpcre3-dev
+        ./configure --prefix=/usr --without-clisp --without-maximum-compile-warnings
+        sudo make && sudo make install
+        sudo install -v -m755 -d /usr/share/doc/swig-3.0.10
+        sudo cp -v -R Doc/* /usr/share/doc/swig-3.0.10
+        sudo apt-get install -y libatlas-base-dev
+        sudo git clone ${snowboy} ~/snowboy
+        cd ~/snowboy/swig/Python3
+        sudo make
+        sudo cp _snowboydetect.so ~/wukong-robot/snowboy/
+        sudo rm -rf ~/install_temp/ ~/snowboy/
+        sed -i '$a export PULSE_SERVER=tcp:localhost' ~/.bashrc
+        source ~/.bash_profile
         sudo chmod -R 777 ~/wukong-robot/
     fi
 clear
@@ -105,16 +135,6 @@ then
     else
         install_wukong "macos"
     fi
-elif [[ ${os_name[0]} == "Linux" && ${os_name[1]} != "raspberrypi" ]]
-then
-    read -p "您的系统是否为Linux? [Y/n]:" confirm
-    if [[ ${confirm} != "Y" && ${confirm} != "y" && ${confirm} != "" ]]
-    then
-        echo "系统检测错误，已退出安装，请反馈给作者"
-        exit
-    else
-        install_wukong "linux"
-    fi
 elif [[ ${os_name[0]} == "Linux" && ${os_name[1]} == "raspberrypi" ]]
 then   
     read -p "您的系统是否为Raspbian? [Y/n]:" confirm    
@@ -125,4 +145,23 @@ then
     else                      
         install_wukong "raspberrypi"
     fi 
+elif [[ ${os_name[0]} == "Linux" && ${os_name[2]} =~ "Microsoft" ]]
+then   
+    read -p "您的系统是否为win10子系统? [Y/n]:" confirm    
+    if [[ ${confirm} != "Y" && ${confirm} != "y" && ${confirm} != "" ]]
+    then                 
+        echo "系统检测错误，已退出安装，请反馈给作者"
+        exit             
+    else
+        install_wukong "win10"
+    fi
+elif [[ ${os_name[0]} == "Linux" && ${os_name[1]} != "raspberrypi" ]]
+then
+    if [[ ${confirm} != "Y" && ${confirm} != "y" && ${confirm} != "" ]]
+    then                                                                                                                   
+        echo "系统检测错误，已退出安装，请反馈给作者"
+        exit
+    else
+        install_wukong "linux"
+    fi
 fi
