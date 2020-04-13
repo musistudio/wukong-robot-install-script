ef6245668ffdd6ee936e63806bdbbfad32cda6da
musistudio <musiteam@musiiot.top>
2020-04-13 13:52:37 +0800

    换源脚本适配最新版系统

--- changeSource.sh
+++ changeSource.sh
@@ -1,7 +1,16 @@
 # /bin/bash
 
+sudo dpkg-reconfigure dash
+version=`lsb_release -a`
+OLD_IFS="$IFS"
+IFS=":"
+array=($version)
+IFS="$OLD_IFS"
+len=${#array[*]}
+v=${array[$len-1]}
+
 sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak && sudo mv /etc/apt/sources.list.d/raspi.list /etc/apt/sources.list.d/raspi.list.bak
-echo 'deb http://mirrors.ustc.edu.cn/raspbian/raspbian/ stretch main contrib non-free rpi' | sudo tee /etc/apt/sources.list
-echo 'deb http://mirrors.ustc.edu.cn/archive.raspberrypi.org/debian/ stretch main ui' | sudo tee /etc/apt/sources.list.d/raspi.list.bak
+echo 'deb http://mirrors.ustc.edu.cn/raspbian/raspbian/'$v' main contrib non-free rpi' | sudo tee /etc/apt/sources.list
+echo 'deb http://mirrors.ustc.edu.cn/archive.raspberrypi.org/debian/'$v' main ui' | sudo tee /etc/apt/sources.list.d/raspi.list
 echo "文件修改成功!"
 sudo apt-get update && sudo apt-get -y upgrade
